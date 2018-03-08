c14e2a5261060255a06acd26949acd47e3836a01
ismurray <ians.murray@yahoo.com>
2018-03-08 17:22:23 -0500

    Initial Game Logic, initialize map, set neighbors
    - Create scripts/game/engine.js file to house the game engine
    - Create createMap func to initialize map as 2D array based on given
      row length, all spaces are initialized empty as '   '
    - Create sample Game State data as it might come from the API: over
      (boolean), score (number), playerState (hash of important player
      values), goblinStates (array of hashes of each goblin's important
      values)
    - Create function setNeighborIndices to create a list of indices of the
      neighboring cells of a given combatant
    - Create function validateNeighborIndices to set any neighbor indices
      that correspond to a location outside of the map to 'wall'

---
+++ assets/scripts/game/engine.js
@@ -0,0 +1,71 @@
+'use strict'
+
+// initial variables
+const rowLength = 5
+let map = []
+// playerStartPos = [4, 2]
+// gobStartPos = [0, 3]
+
+// Sample game state data that could come from API:
+let score = 0
+let over = false
+let playerState = {
+  name: 'player',
+  position: [4, 2],
+  hp: [10, 10],
+  attack: 5,
+  alive: true
+}
+let goblinStates = [
+  {
+    name: 'goblin',
+    position: [0, 3],
+    hp: [10, 10],
+    attack: 2,
+    alive: true
+  }
+]
+
+// initializes the game map to be a 2D array
+const createMap = function (rowLength) {
+  const row = []
+  for (let i = 0; i < rowLength; i++) {
+    row.push('   ')
+  }
+  for (let i = 0; i < rowLength; i++) {
+    map.push(row)
+  }
+}
+
+// Player funcs:
+
+// Creates a hash of the indices of all four neighboring cells for a given combatant
+const setNeighborIndices = function (combatant) {
+  combatant.neighborIndices = {
+    up: [combatant.position[0] - 1, combatant.position[1]],
+    down: [combatant.position[0] + 1, combatant.position[1]],
+    left: [combatant.position[0], combatant.position[1] - 1],
+    right: [combatant.position[0], combatant.position[1] + 1]
+  }
+  validateNeighborIndices(combatant)
+}
+
+// Set any neighborIndices that are outside the map to 'wall'
+const validateNeighborIndices = function (combatant) {
+  // If player is in top row, set up to 'wall'
+  if (combatant.position[0] === 0) {
+    combatant.neighborIndices.up = 'wall'
+  }
+  // If player is in bottom row, set down to 'wall'
+  if (combatant.position[0] === rowLength - 1) {
+    combatant.neighborIndices.down = 'wall'
+  }
+  // If player is at far left, set left to 'wall'
+  if (combatant.position[1] === 0) {
+    combatant.neighborIndices.left = 'wall'
+  }
+  // If player is at far right, set right to 'wall'
+  if (combatant.position[1] === rowLength - 1) {
+    combatant.neighborIndices.right = 'wall'
+  }
+}
